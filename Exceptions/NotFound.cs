dcb8175b740bc08fd544f2caca506fd315ad52f8
habiba alaa <habibaalaa775@gmal.com>
2025-07-29 11:34:08 +0300

    edited in global exception handler and added not foundhandler

---
+++ Exceptions/NotFound.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace week2_Task.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message)
+        {
+        }
+    }
+}

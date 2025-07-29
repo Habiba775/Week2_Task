8e5a28a35cf94a82fe6849ceba4730462a950916
habiba alaa <habibaalaa775@gmal.com>
2025-07-29 11:41:18 +0300

    Inject logging  and serilog

--- Program.cs
+++ Program.cs
@@ -9,8 +9,74 @@ using week2_Task.Models;
 using week2_Task.Repositories;
 using week2_Task.Services;
 using System;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Sinks.MSSqlServer;
+using week2_Task.MiddleWares;
+using week2_Task.Exceptions;
+
+
 
 var builder = WebApplication.CreateBuilder(args);
+
+
+
+
+
+
+//serilog 
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .WriteTo.File(
+        path: "Logs/log-.txt",
+        rollingInterval: RollingInterval.Day,
+        outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}"
+    )
+    .CreateLogger();
+
+// Use Serilog for logging
+builder.Host.UseSerilog();
+
+
+
+
+///////////////////////////////////
+//json format in console but parsed
+
+builder.Logging.AddJsonConsole(options =>
+{
+    
+    options.JsonWriterOptions = new System.Text.Json.JsonWriterOptions
+    {
+        Indented = true                 
+    };
+}); 
+/////////////////////
+
+
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.MSSqlServer(
+        connectionString: builder.Configuration.GetConnectionString("DefaultConnection"),
+        sinkOptions: new MSSqlServerSinkOptions
+        {
+            TableName = "Logs",
+            AutoCreateSqlTable = true 
+        })
+    .CreateLogger();
+///http logging
+
+
+
+
+builder.Services.AddHttpLogging(options => { });
+
 
 // Add services to the container
 builder.Services.AddControllers()
@@ -21,6 +87,12 @@ builder.Services.AddControllers()
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
+
+
+builder.Services.AddExceptionHandler<NotFoundExceptionHandler>();
 
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
@@ -75,7 +147,20 @@ builder.Services.AddScoped<IPaymentService, PaymentService>();
 builder.Services.AddScoped<LinqDemoService>();
 builder.Services.AddSingleton<TokenService>();
 
+
+
+
+
+
+
+
+
+
 var app = builder.Build();
+app.UseHttpLogging();
+
+
+
 
 //example of db connection
 using (var scope = app.Services.CreateScope())
@@ -96,8 +181,9 @@ using (var scope = app.Services.CreateScope())
 
 //just msg
 
+
 app.Logger.LogInformation(5, "db is ready");
-app.Logger.LogInformation(5, "db is ready");
+app.Logger.LogWarning(5, "db is ready");
 
 // Role seeding like the first register
 using (var scope = app.Services.CreateScope())
@@ -113,13 +199,16 @@ using (var scope = app.Services.CreateScope())
     }
 }
 
+
 // Configure HTTP pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
+app.UseExceptionHandler();
 app.UseHttpsRedirection();
 app.UseCors("AllowAll");
 

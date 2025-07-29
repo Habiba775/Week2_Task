700961d59b16a3df4df506d0a19d4d40c4104df6
habiba alaa <habibaalaa775@gmal.com>
2025-07-29 11:36:35 +0300

    Add warning log for not found Payment ID in PaymentRepository

--- Repositories/PaymentRepository.cs
+++ Repositories/PaymentRepository.cs
@@ -1,24 +1,37 @@
 ﻿using week2_Task.Data;
 using week2_Task.Models.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 public class PaymentRepository : IPaymentRepository
 {
     private readonly ApplicationDBContext _context;
+    private readonly ILogger<PaymentRepository> _logger;
 
-    public PaymentRepository(ApplicationDBContext context)
+    public PaymentRepository(ApplicationDBContext context, ILogger<PaymentRepository> logger)
     {
         _context = context;
+        _logger = logger;
     }
 
     public async Task<Payment> GetByIdAsync(Guid id)
     {
-        return await _context.Payments.Include(p => p.Merchant).FirstOrDefaultAsync(p => p.PaymentId == id);
+        var payment = await _context.Payments.Include(p => p.Merchant).FirstOrDefaultAsync(p => p.PaymentId == id);
+
+        //logging in case of no payment
+        if (payment == null)
+        {
+            _logger.LogWarning("Payment with ID {PaymentId} not found.", id);
+        }
+       
+        return payment;
+
     }
 
     public async Task<List<Payment>> GetAllAsync()
     {
         return await _context.Payments.Include(p => p.Merchant).ToListAsync();
+
     }
 
     public async Task AddAsync(Payment payment)
